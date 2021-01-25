acf32bf536c425ada2c8486c1aa23e10bebc99ba
Nico Mastakas <nicolasmastakas@gmail.com>
2021-01-25 16:32:39 -0300

    nasa

---
+++ src/components/NasaPhoto.js
@@ -0,0 +1,46 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import NavBar from "./NavBar";
+
+const apiKey = process.env.REACT_APP_NASA_KEY;
+
+export default function NasaPhoto() {
+  const [photoData, setPhotoData] = useState(null);
+  useEffect(() => {
+    fetchPhoto();
+    async function fetchPhoto() {
+      const res = await fetch(
+        `https://api.nasa.gov/planetary/apod?api_key=${apiKey}`
+      );
+      const data = await res.json();
+      setPhotoData(data);
+    }
+  }, []);
+  if (!photoData) return <div />;
+  return (
+    <div>
+      <NavBar />
+      <div className="nasa-photo">
+        {photoData.media_type === "image" ? (
+          <img src={photoData.url} className="photo" />
+        ) : (
+          <iframe
+            title=""
+            src={photoData.url}
+            frameBorder="0"
+            gesture="media"
+            allow="encrypted-media"
+            allowFullScreen
+            className="photo"
+          />
+        )}
+
+        <div>
+          <h1>{photoData.title}</h1>
+          <p className="date">{photoData.data}</p>
+          <p className="explanation">{photoData.explanation}</p>
+        </div>
+      </div>
+    </div>
+  );
+}
