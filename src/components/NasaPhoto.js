c9d042abb8fe08fa2098ca9f0a2d30b46a3d7d49
mstefa <mstefanutti24@gmail.com>
2021-01-30 11:55:00 -0300

    Pic of the Day with styled added

--- src/components/NasaPhoto.js
+++ src/components/NasaPhoto.js
@@ -1,9 +1,11 @@
 import React from "react";
 import { useState, useEffect } from "react";
 import NavBar from "./NavBar";
-
+import { Link } from "react-router-dom";
+import Style from './NasaPhoto.module.css'
+import {apiKeyNasa } from '../config'
 // const apiKey = process.env.REACT_APP_NASA_KEY;
-const apiKey = 'oehYU5iXzd3RaWUhgGCSI1M79bqvPabNVQf51A5j';
+
 
 export default function NasaPhoto() {
   const [photoData, setPhotoData] = useState(null);
@@ -11,20 +13,25 @@ export default function NasaPhoto() {
     fetchPhoto();
     async function fetchPhoto() {
       const res = await fetch(
-        `https://api.nasa.gov/planetary/apod?api_key=${apiKey}`
+        `https://api.nasa.gov/planetary/apod?api_key=${apiKeyNasa}`
       );
       const data = await res.json();
       setPhotoData(data);
     }
   }, []);
+
   if (!photoData) return <div />;
+
   return (
     <div>
       {console.log(photoData)}
-      <NavBar />
+      {/* <NavBar /> */}
+      <Link className="link" to="">
+            back to earth
+      </Link>
       <div className="nasaphoto">
-        {photoData.media_type === "image" ? (
-          <img src={photoData.url} className="photo" alt='nice'/>
+        {/* {photoData.media_type === "image" ? (
+          <img src={photoData.url} className={Style.photo} alt='nice'/>
         ) : (
           <iframe
             title="Picture of the day"
@@ -35,12 +42,13 @@ export default function NasaPhoto() {
             allowFullScreen
             className="photo"
           />
-        )}
+        )} */}
 
-        <div>
-          <h1>{photoData.title}</h1>
-          <p className="date">{photoData.data}</p>
-          <p className="explanation">{photoData.explanation}</p>
+        <div className={Style.container}>
+          <img src={photoData.url} className={Style.photo} alt='nice'/>
+          <h1 className={Style.title}>{photoData.title}</h1>
+          <p className={Style.date}>{photoData.date}</p>
+          <p className={Style.explanation}>{photoData.explanation}</p>
         </div>
       </div>
     </div>
