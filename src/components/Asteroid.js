9414a42736aa5cd811c7205746e6d5214c84e827
mstefa <mstefanutti24@gmail.com>
2021-01-31 11:47:10 -0300

    Asteroid near earth added

--- src/components/Asteroid.js
+++ src/components/Asteroid.js
@@ -1,8 +1,20 @@
 import React from 'react'
+import Style from './Asteroid.module.css'
+
 
 export default function Asteroid(props) {
+  
+  function toKilometers(num){
+    return (' ' +  num.toFixed(2) + ' ' + 'Km.' + ' ');
+  }
+
+  function toKilometersPerHour(num){
+    num = parseInt(num)
+    return (' ' +  num.toFixed(2) + ' ' + 'Km/hr.' + ' ');
+  }
+
   return (
-    <div>
+    <div className={Style.container}>
       <div>
         <span>ID:{' '}</span> 
         <span>{props.data.id}</span>
@@ -13,24 +25,16 @@ export default function Asteroid(props) {
       </div>
       <div>
         <span>velocidad de aproximacion:{' '}</span> 
-        <span>{props.data.close_approach_data[0].relative_velocity.kilometers_per_hour}</span>
+        <span>{toKilometersPerHour(props.data.close_approach_data[0].relative_velocity.kilometers_per_hour)}</span>
       </div>
       <div>
         <span>Tamaño:{' '}</span> 
         <span>Diametro minimo:{' '}</span> 
-        <span>{props.data.estimated_diameter.kilometers.estimated_diameter_min}</span>
+        <span>{toKilometers(props.data.estimated_diameter.kilometers.estimated_diameter_min)}</span>
         <span>Diametro maximo:{' '}</span> 
-        <span>{props.data.estimated_diameter.kilometers.estimated_diameter_max}</span>
+        <span>{toKilometers(props.data.estimated_diameter.kilometers.estimated_diameter_max)}</span>
       </div>
 
     </div>
   )
 }
-
-
-// data.map((e)=>console.log(e.estimated_diameter.kilometers.estimated_diameter_min))
-// data.map((e)=>console.log(e.estimated_diameter.kilometers.estimated_diameter_max))
-// data.map((e)=>console.log(e.id))
-// data.map((e)=>console.log(e.close_approach_data[0].close_approach_date_full))
-// data.map((e)=>console.log(e.close_approach_data[0].relative_velocity.kilometers_per_hour))
-// data.map((e)=>console.log(e.estimated_diameter.kilometers.estimated_diameter_max))
